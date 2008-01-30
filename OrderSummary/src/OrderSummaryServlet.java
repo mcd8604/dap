a7018247be65a32306a8fd0af3830cc9292e4c07
mcd8604 <mcd8604@users.noreply.github.com>
2008-01-30 04:52:24 +0000

    added servlet

---
+++ OrderSummary/src/OrderSummaryServlet.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class for Servlet: OrderSummaryServlet
+ *
+ */
+ public class OrderSummaryServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+   static final long serialVersionUID = 1L;
+   
+    /* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public OrderSummaryServlet() {
+		super();
+	}   	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Order> orders = DatabaseController.getOrdersToday();
+		ArrayList<Customer> customers = DatabaseController.getCustomersToday();
+		ArrayList<Item> items = DatabaseController.getItemsWithSuppliers();
+	}  	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}   	  	    
+}
