13cb921ff7edb9b1b20f724f52c3e286ffc3140e
mcd8604 <mcd8604@users.noreply.github.com>
2008-02-17 18:43:38 +0000

    oops

--- OrderSummary/src/AccountServlet.java
+++ OrderSummary/src/AccountServlet.java
@@ -2,12 +2,10 @@
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class for Servlet: AccountServlet
@@ -28,11 +26,6 @@ import javax.servlet.http.HttpSession;
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
-		Enumeration e = request.getAttributeNames();
-		Object o = null;
-		while((o = e.nextElement()) != null) {
-			System.out.println(o.toString());
-		}
 	}  	
 	
 	/* (non-Java-doc)
@@ -40,25 +33,19 @@ import javax.servlet.http.HttpSession;
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		if (request.getAttribute("action").equals("getCustomer")) {
-			// Create Customer c from JSP page
-			Customer c = null;
-			DatabaseController.isCustomer(c);
-			HttpSession sess = request.getSession();
-			sess.
-		} else if (request.getAttribute("action").equals("createCustomer")) {
+		if (request.getAttribute("action").equals("new account")) {
 			// Create Customer c from JSP page
 			Customer c = null;
 			DatabaseController.createCustomer(c);
-		} else if (request.getAttribute("action").equals("editCustomer")) {
+		} else if (request.getAttribute("action").equals("edit account")) {
 			// Create Customer c from JSP page
 			Customer c = null;
 			DatabaseController.editCustomer(c);
-		} else if (request.getAttribute("action").equals("removeCustomer")) {
+		} else if (request.getAttribute("action").equals("delete account")) {
 			// Create Customer c from JSP page
 			Customer c = null;
 			DatabaseController.deleteCustomer(c);
-		} else if (request.getAttribute("action").equals("getCustomerOrders")) {
+		} else if (request.getAttribute("action").equals("order history")) {
 			// Create Customer c from JSP page
 			Customer c = null;
 			DatabaseController.getOrders(c);
